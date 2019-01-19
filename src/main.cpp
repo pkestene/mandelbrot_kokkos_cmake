19e5c0fee7a4526c9308c3191fa1f554fdfdde7f
Pierre Kestener <pierre.kestener@cea.fr>
2019-01-19 18:10:32 +0100

    move computation outside of main

--- src/main.cpp
+++ src/main.cpp
@@ -22,41 +22,17 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-
-  /*
-   * Initialize kokkos (host + device)
-   * 
-   * If CUDA is enabled, Kokkos will try to use the default GPU, i.e. GPU #0 if you
-   * have multiple GPUs.
-   */
-  Kokkos::initialize(argc, argv);
-
-  {
-    std::cout << "##########################\n";
-    std::cout << "KOKKOS CONFIG             \n";
-    std::cout << "##########################\n";
-    
-    std::ostringstream msg;
-    std::cout << "Kokkos configuration" << std::endl;
-    if ( Kokkos::hwloc::available() ) {
-      msg << "  hwloc( NUMA[" << Kokkos::hwloc::get_available_numa_count()
-          << "  ] x CORE["    << Kokkos::hwloc::get_available_cores_per_numa()
-          << "  ] x HT["      << Kokkos::hwloc::get_available_threads_per_core()
-          << "  ] )"
-          << std::endl ;
-    }
-    Kokkos::print_configuration( std::cout );
-    std::cout << msg.str();
-    std::cout << "##########################\n";
-  }
+// ==================================================================
+// ==================================================================
+// ==================================================================
+void compute_mandelbrot_set(int argc, char* argv[]) {
 
 #ifdef KOKKOS_ENABLE_CUDA
   CudaTimer timer;
 #else
   OpenMPTimer timer;
 #endif
-
+  
   int default_size = 8192;
   if (argc>1)
     default_size = std::atoi(argv[1]);
@@ -101,6 +77,41 @@ int main(int argc, char* argv[]) {
     save_ppm(imageHost, filename, constants);
   }
   printf("Compute time: %lf seconds.\n", timer.elapsed());
+
+} // compute_mandelbrot_set
+
+//============================================================
+//============================================================
+int main(int argc, char* argv[]) {
+
+  /*
+   * Initialize kokkos (host + device)
+   * 
+   * If CUDA is enabled, Kokkos will try to use the default GPU, i.e. GPU #0 if you
+   * have multiple GPUs.
+   */
+  Kokkos::initialize(argc, argv);
+
+  {
+    std::cout << "##########################\n";
+    std::cout << "KOKKOS CONFIG             \n";
+    std::cout << "##########################\n";
+    
+    std::ostringstream msg;
+    std::cout << "Kokkos configuration" << std::endl;
+    if ( Kokkos::hwloc::available() ) {
+      msg << "  hwloc( NUMA[" << Kokkos::hwloc::get_available_numa_count()
+          << "  ] x CORE["    << Kokkos::hwloc::get_available_cores_per_numa()
+          << "  ] x HT["      << Kokkos::hwloc::get_available_threads_per_core()
+          << "  ] )"
+          << std::endl ;
+    }
+    Kokkos::print_configuration( std::cout );
+    std::cout << msg.str();
+    std::cout << "##########################\n";
+  }
+
+  compute_mandelbrot_set(argc,argv);  
      
   Kokkos::finalize();
 
